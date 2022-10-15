9b026d8eda180ea3f515fa560fb6247c6e6db806
Sergio <ccdsergio@gmail.com>
2022-10-15 10:18:48 -0400

    Ajouter Securite Interne

---
+++ src/main/java/guru/springframework/basicauth/controller/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package guru.springframework.basicauth.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        auth.inMemoryAuthentication()
+            .withUser(user);
+    }
+}
